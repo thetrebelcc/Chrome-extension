ce0a5cb98916d00c165b8fb6230dc85acae0d60b
tezzsurya <tejasurya1729@gmail.com>
2016-12-04 17:29:11 +0530

    Adding CloseThisTab Extension

---
+++ popup.js
@@ -0,0 +1,1 @@
+chrome.contextMenus.create({title:"Close this Tab",contexts:["all"],});chrome.contextMenus.onClicked.addListener(function(){chrome.tabs.query({currentWindow:true,active:true},function(tabs){chrome.tabs.remove(tabs[0].id);});});

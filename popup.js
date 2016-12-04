638587ba62fccd8a5ec615d0394c66913bc45b08
tezzsurya <tejasurya1729@gmail.com>
2016-12-04 18:16:23 +0530

    Adding CloseThisTab all files

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-chrome.contextMenus.create({title:"Close this Tab",contexts:["all"],});chrome.contextMenus.onClicked.addListener(function(){chrome.tabs.query({currentWindow:true,active:true},function(tabs){chrome.tabs.remove(tabs[0].id);});});
+chrome.contextMenus.create( {
+	title: "Close this Tab",
+	contexts: ["all"],
+});
+
+chrome.contextMenus.onClicked.addListener(function() {
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  			chrome.tabs.remove(tabs[0].id);
+	});
+});
